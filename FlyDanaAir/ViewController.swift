1ab678f4137a4703b2a83cdc576d69f817a5b0fb
Ese Bobori <EseBobori@eses-mbp.home>
2015-04-21 20:58:00 +0100

    Commiting using Terminal

--- FlyDanaAir/ViewController.swift
+++ FlyDanaAir/ViewController.swift
@@ -10,18 +10,6 @@ import UIKit
 
 class ViewController: UIViewController, UIWebViewDelegate, NSURLConnectionDelegate {
     
-    
-//    func connection(connection: NSURLConnection, willSendRequestForAuthenticationChallenge challenge: NSURLAuthenticationChallenge) {
-//        
-//        
-//        println("Webcontroller got auth challenge via nsurlconnection")
-//        
-//        if (challenge.previousFailureCount == 0)
-//        {
-//            var credential = NSURLCredential(forTrust: challenge.protectionSpace.serverTrust)
-//        }
-//        
-//    }
 
     
 
